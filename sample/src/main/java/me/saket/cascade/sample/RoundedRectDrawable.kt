39dd9aae6273c4906744f83bf187625f190c527b
geftimov <jokatavr@gmail.com>
2022-05-05 16:38:23 +0100

    Develop

--- sample/src/main/java/me/saket/cascade/sample/RoundedRectDrawable.kt
+++
@@ -1,9 +0,0 @@
-package me.saket.cascade.sample
-
-import android.graphics.drawable.PaintDrawable
-
-class RoundedRectDrawable(color: Int, radius: Float) : PaintDrawable(color) {
-  init {
-    setCornerRadius(radius)
-  }
-}
